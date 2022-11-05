90675ddac3d62dee024ae383cc101f4df531d080
Rico Apon <rico.apon@gmail.com>
2022-11-05 14:16:58 +0100

    Fix issue with typing in random paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {path: '', component: StartScreenComponent},
   {path: 'game', component: PlayGameComponent},
   {path: 'tutorial', component: TutorialComponent},
-  {path: '**', component: StartScreenComponent},
+  {path: '**', redirectTo: '/',},
 ];
 
 @NgModule({
