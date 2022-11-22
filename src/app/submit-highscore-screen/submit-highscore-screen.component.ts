76e46ee62e95484655da1751751606af5000a096
Rico Apon <rico.apon@gmail.com>
2022-11-22 22:52:18 +0100

    Remember highscore name in cookie

--- src/app/submit-highscore-screen/submit-highscore-screen.component.ts
+++ src/app/submit-highscore-screen/submit-highscore-screen.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {HighscoreManager} from "../backend/highscore-manager";
 import {DomSanitizer} from "@angular/platform-browser";
 import {ActivatedRoute, Router} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
 
 @Component({
   selector: 'app-submit-highscore-screen',
@@ -10,9 +11,9 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class SubmitHighscoreScreenComponent implements OnInit {
   state: number
-  name: String
+  name: string
 
-  constructor(private highscoreManager: HighscoreManager, private sanitizer: DomSanitizer, private activatedRoute: ActivatedRoute, private router: Router) {
+  constructor(private highscoreManager: HighscoreManager, private sanitizer: DomSanitizer, private activatedRoute: ActivatedRoute, private router: Router, private cookieService: CookieService) {
     if (this.router.getCurrentNavigation() !== undefined && this.router.getCurrentNavigation()!.extras !== undefined && this.router.getCurrentNavigation()!.extras.state !== undefined) {
       this.state = this.router.getCurrentNavigation()!.extras.state!['highscore']
     } else {
@@ -22,9 +23,14 @@ export class SubmitHighscoreScreenComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    const highscoreName = this.cookieService.get('highscore_name')
+    if (highscoreName !== undefined && highscoreName.trim().length > 0) {
+      this.name = highscoreName
+    }
   }
 
   submit(): void {
+    this.cookieService.set('highscore_name', this.name, 365)
     this.highscoreManager.addHighscore({name: this.name, score: this.state}).subscribe(
       // Go to the highscore page when submitting succeeded.
       () => this.router.navigate(['/highscore'])
